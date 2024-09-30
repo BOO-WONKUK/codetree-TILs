2e65218abd4a53137ece40e73843b50e979ce9c9
BOO-WONKUK <102275042+BOO-WONKUK@users.noreply.github.com>
2024-09-30 21:40:37 +0900

    Title: 오름차순으로 정렬되는 수열, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240930/오름차순으로 정렬되는 수열/sequence-sorted-in-ascending-order.py
+++ 240930/오름차순으로 정렬되는 수열/sequence-sorted-in-ascending-order.py
@@ -8,4 +8,7 @@ sort_lst = sorted(lst)
 for i in range(n):
     if lst[i] != sort_lst[i]:
         result.append(i)
-print(max(result) - min(result) - 1)
+if max(result) == n-1:
+    print(max(result) - min(result) - 1)
+else:
+    print(max(result) - min(result))
