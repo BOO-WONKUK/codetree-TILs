4dd40af80bcf4f012beea38072ae5ee1d1560bd5
BOO-WONKUK <102275042+BOO-WONKUK@users.noreply.github.com>
2024-09-30 23:29:33 +0900

    Title: 이상한 진수, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240930/이상한 진수/awkward-digits.py
+++ 240930/이상한 진수/awkward-digits.py
@@ -1,22 +1,20 @@
-a = input()
-b = input()
-int_a = int(a,2)
-int_b = int(b,3)
-sub = abs(int_a - int_b)
-tmp = -1
+a = input().strip()
+b = input().strip()
+
+int_a = int(a, 2)
+int_b = int(b, 3)
+
 for i in range(len(a)):
     for j in range(len(b)):
-        if sub == abs(2**i - 3**j) or sub == 2**i + 3**j:
-            tmp = 0
-            break
-    if tmp == 0:
-        break
-
-if int_a + 2**i == int_b + 3**j:
-    print(int_a + 2**i)
-elif int_a - 2**i == int_b + 3**j:
-    print(int_a - 2**i)
-elif int_a + 2**i == int_b - 3**j:
-    print(int_a + 2**i)
-elif int_a - 2**i == int_b - 3**j:
-    print(int_a - 2**i)
+        a_plus = int_a + (2**i) 
+        a_minus = int_a - (2**i)
+        
+        b_plus = int_b + (3 ** j)
+        b_minus = int_b - (3 ** j)
+        
+        if a_plus == b_plus or a_plus == b_minus:
+            print(a_plus)
+            exit()
+        elif a_minus == b_plus or a_minus == b_minus:
+            print(a_minus)
+            exit()
