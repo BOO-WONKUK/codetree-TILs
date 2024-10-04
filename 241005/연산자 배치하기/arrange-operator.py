21244fc90680b2718478ac109e0b6e0d804146b9
BOO-WONKUK <102275042+BOO-WONKUK@users.noreply.github.com>
2024-10-05 01:56:34 +0900

    Title: 연산자 배치하기, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241005/연산자 배치하기/arrange-operator.py
@@ -0,0 +1,30 @@
+from itertools import permutations
+
+n = int(input())
+numbers = list(map(int, input().split()))
+operators = list(map(int, input().split()))
+
+operator_list = []
+operator_list += ['+'] * operators[0]
+operator_list += ['-'] * operators[1]
+operator_list += ['*'] * operators[2]
+
+all_permutations = set(permutations(operator_list))
+
+min_value = 1e9  
+max_value = -1e9 
+
+for perm in all_permutations:
+    result = numbers[0]
+    for i in range(1, n):
+        if perm[i-1] == '+':
+            result += numbers[i]
+        elif perm[i-1] == '-':
+            result -= numbers[i]
+        elif perm[i-1] == '*':
+            result *= numbers[i]
+    
+    min_value = min(min_value, result)
+    max_value = max(max_value, result)
+
+print(min_value, max_value)
