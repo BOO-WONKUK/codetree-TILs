7819f7f5956d39261caac0011c59b3cbdde34e0f
BOO-WONKUK <102275042+BOO-WONKUK@users.noreply.github.com>
2024-10-06 16:17:24 +0900

    Title: 조삼모사, Time: 5079ms, Memory: 48MB, Status: Time Limit Exceed - CodeTree

--- 241006/조삼모사/three-at-dawn-and-four-at-dusk.py
+++ 241006/조삼모사/three-at-dawn-and-four-at-dusk.py
@@ -14,23 +14,22 @@ for j in com:
         lst_com.append(list(j))
 
 for j in lst_com:
-    # print('---------------------------')
     tmp = set(i for i in range(1, n + 1))
-    # print(j)
-    # print(tmp)
     k = list(tmp - set(j))
-    work_com_morn = list(permutations(j,2))
-    work_com_night = list(permutations(k,2))
-    # print(work_com_morn)
-    # print(work_com_night)
+
+    work_com_morn = list(combinations(j,2))
+    work_com_night = list(combinations(k,2))
+
     morn_sum = 0
     night_sum = 0
+
     for x,y in work_com_morn:
         morn_sum += lst[y-1][x-1]
+        morn_sum += lst[x - 1][y - 1]
     for x,y in work_com_night:
         night_sum += lst[y-1][x-1]
-    # print('m_sum', morn_sum)
-    # print('n_sum', night_sum)
+        night_sum += lst[x - 1][y - 1]
+
     result.append(abs(morn_sum - night_sum))
 
 
