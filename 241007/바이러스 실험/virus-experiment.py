17e5c5e4975bd73c20ef5de726c0e2f8b2ef147e
BOO-WONKUK <102275042+BOO-WONKUK@users.noreply.github.com>
2024-10-07 22:30:08 +0900

    Title: 바이러스 실험, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 241007/바이러스 실험/virus-experiment.py
@@ -0,0 +1,49 @@
+n,m,k = map(int, input().split())
+yang = [[5]*5 for _ in range(5)]
+yang_plus = [list(map(int, input().split())) for _ in range(5)]
+vir = []
+for _ in range(m):
+    r,c,age = map(int, input().split())
+    vir.append([r-1,c-1,age])
+
+def is_range(r,c):
+    if 0<=r<=4 and 0<=c<=4:
+        return True
+    return False
+
+def simulate():
+    vir.sort(key = lambda x : x[2])
+    lst_del = []
+    for i in range(len(vir)):
+        r = vir[i][0]
+        c = vir[i][1]
+        age = vir[i][2]
+        if yang[r][c] >= age:
+            yang[r][c] -= age
+            vir[i][2] += 1
+        else:
+            lst_del.append(i)
+    for i in lst_del:
+        yang[vir[i][0]][vir[i][1]] += vir[i][2]//2
+    for i in reversed(lst_del):
+        del vir[i]
+    for i in range(len(vir)):
+        r = vir[i][0]
+        c = vir[i][1]
+        age = vir[i][2]
+        if age % 5 == 0:
+            for k in range(-1,2):
+                for l in range(-1,2):
+                    if is_range(r+k,c+l):
+                        if l==0 and k == 0:
+                            pass
+                        else:
+                            vir.append([r+k,c+l,1])
+    for i in range(5):
+        for j in range(5):
+            yang[i][j] += yang_plus[i][j]
+
+for _ in range(k):
+    simulate()
+
+print(len(vir))
