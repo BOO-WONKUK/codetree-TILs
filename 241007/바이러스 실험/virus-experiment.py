9c32eaab8adae53598440ffd0fe8d31ba89254fd
BOO-WONKUK <102275042+BOO-WONKUK@users.noreply.github.com>
2024-10-07 23:18:02 +0900

    Title: 바이러스 실험, Time: 1512ms, Memory: 29MB, Status: Passed - CodeTree

--- 241007/바이러스 실험/virus-experiment.py
+++ 241007/바이러스 실험/virus-experiment.py
@@ -1,80 +1,60 @@
-n,m,k = map(int, input().split())
-yang = [[5]*n for _ in range(n)]
-yang_plus = [list(map(int, input().split())) for _ in range(n)]
-vir = []
+from collections import deque
+
+n, m, k = map(int, input().split())
+yang = [[5] * n for _ in range(n)]  # 양분 초기값
+yang_plus = [list(map(int, input().split())) for _ in range(n)]  # 매년 추가되는 양분
+vir_map = [[deque() for _ in range(n)] for _ in range(n)]  # 각 칸에 있는 나무들(큐로 관리)
+
+# 나무 입력 받기
 for _ in range(m):
-    r,c,age = map(int, input().split())
-    vir.append([r-1,c-1,age])
+    r, c, age = map(int, input().split())
+    vir_map[r-1][c-1].append(age)
 
-def is_range(r,c):
-    if 0<=r<=n-1 and 0<=c<=n-1:
-        return True
-    return False
+# 방향 배열 (8방향)
+directions = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
 
-# def simulate():
-#     vir.sort(key = lambda x : x[2])
-#     lst_del = []
-#     for i in range(len(vir)):
-#         r = vir[i][0]
-#         c = vir[i][1]
-#         age = vir[i][2]
-#         if yang[r][c] >= age:
-#             yang[r][c] -= age
-#             vir[i][2] += 1
-#         else:
-#             lst_del.append(i)
-#     for i in lst_del:
-#         yang[vir[i][0]][vir[i][1]] += vir[i][2]//2
-#     for i in reversed(lst_del):
-#         del vir[i]
-#     for i in range(len(vir)):
-#         r = vir[i][0]
-#         c = vir[i][1]
-#         age = vir[i][2]
-#         if age % 5 == 0:
-#             for k in range(-1,2):
-#                 for l in range(-1,2):
-#                     if is_range(r+k,c+l):
-#                         if l==0 and k == 0:
-#                             pass
-#                         else:
-#                             vir.append([r+k,c+l,1])
-#     for i in range(n):
-#         for j in range(n):
-#             yang[i][j] += yang_plus[i][j]
 def simulate():
-    vir.sort(key=lambda x: x[2])  # 매번 정렬 필요 없을 경우 제거 고려
-    new_vir = []  # 살아남은 나무를 기록할 리스트
-    breed_queue = []  # 새로 추가할 나무 위치를 기록할 리스트
-    for r, c, age in vir:
-        if yang[r][c] >= age:
-            yang[r][c] -= age
-            age += 1
-            new_vir.append([r, c, age])
-            if age % 5 == 0:
-                breed_queue.append((r, c))
-        else:
-            yang[r][c] += age // 2  # 죽은 나무 처리
-    # 새로운 나무 번식 처리
-    for r, c in breed_queue:
-        for dr in range(-1, 2):
-            for dc in range(-1, 2):
-                if (dr, dc) != (0, 0) and is_range(r + dr, c + dc):
-                    new_vir.append([r + dr, c + dc, 1])
-    # 양분 추가
+    # 봄 & 여름
+    for i in range(n):
+        for j in range(n):
+            if vir_map[i][j]:
+                live_trees = deque()  # 살아남을 나무들
+                dead_nutrients = 0  # 죽은 나무가 남긴 양분
+                
+                while vir_map[i][j]:
+                    age = vir_map[i][j].popleft()
+                    if yang[i][j] >= age:  # 나이가 양분보다 적거나 같으면 성장
+                        yang[i][j] -= age
+                        live_trees.append(age + 1)
+                    else:
+                        dead_nutrients += age // 2  # 죽은 나무가 남긴 양분
+
+                yang[i][j] += dead_nutrients  # 여름에 죽은 나무들의 양분 추가
+                vir_map[i][j] = live_trees  # 살아남은 나무만 다시 저장
+
+    # 가을 (번식)
+    for i in range(n):
+        for j in range(n):
+            for age in vir_map[i][j]:
+                if age % 5 == 0:  # 나이가 5의 배수인 나무만 번식
+                    for dr, dc in directions:
+                        nr, nc = i + dr, j + dc
+                        if 0 <= nr < n and 0 <= nc < n:
+                            vir_map[nr][nc].appendleft(1)  # 번식하는 나무의 나이는 1
+
+    # 겨울 (양분 추가)
     for i in range(n):
         for j in range(n):
             yang[i][j] += yang_plus[i][j]
-    return new_vir
 
-# 시뮬레이션 실행
+# k년 동안 시뮬레이션 실행
 for _ in range(k):
-    vir = simulate()
+    simulate()
 
-print(len(vir))
+# 나무의 총 개수 세기
+result = 0
+for i in range(n):
+    for j in range(n):
+        result += len(vir_map[i][j])
 
-
-# for _ in range(k):
-#     simulate()
-
-# print(len(vir))
+print(result)
